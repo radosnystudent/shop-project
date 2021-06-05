2cb51b38dddbb8d5de81f74ecfb49c9ffe38c1c2
radosnystudent <jeremiasz.pauszek@gmail.com>
2021-06-05 15:40:37 +0200

    routing with react-router-dom/bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main className="py-3">
                 <Container>
-                    <HomeScreen />
+                    <Route path="/product/:id" component={ProductScreen} />
+                    <Route exact path="/" component={HomeScreen} />
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 
