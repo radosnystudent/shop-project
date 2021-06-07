ec4c2e46679cc1c58ec41210393aaba1f8f1dcb3
radosnystudent <jeremiasz.pauszek@gmail.com>
2021-06-07 16:43:08 +0200

    initialize reducers, set actions for products

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,18 @@
+import {
+    PRODUCT_LIST_FAILURE,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+} from "../constants/actions";
+
+export const productListReducer = (initialState = { products: [] }, action) => {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST:
+            return { loading: true, products: [] };
+        case PRODUCT_LIST_SUCCESS:
+            return { loading: false, products: action.payload };
+        case PRODUCT_LIST_FAILURE:
+            return { loading: false, error: action.payload };
+        default:
+            return initialState;
+    }
+};
