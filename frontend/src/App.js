29aed6e05e1104c93b9d8022ab8c70bbdbf7df9f
radosnystudent <jeremiasz.pauszek@gmail.com>
2021-06-14 22:13:11 +0200

    add screens to App; CSS form width changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ProductScreen from "./screens/ProductScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const App = () => {
     return (
@@ -20,6 +23,9 @@ const App = () => {
                     <Route path="/login" component={LoginScreen} />
                     <Route path="/register" component={RegisterScreen} />
                     <Route path="/profile" component={ProfileScreen} />
+                    <Route path="/shipping" component={ShippingScreen} />
+                    <Route path="/payment" component={PaymentScreen} />
+                    <Route path="/placeorder" component={PlaceOrderScreen} />
                     <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route exact path="/" component={HomeScreen} />
