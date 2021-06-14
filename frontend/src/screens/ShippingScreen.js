057536208489290a61d72d7a2c133ec67d6d7eb3
radosnystudent <jeremiasz.pauszek@gmail.com>
2021-06-14 20:05:38 +0200

    checkout,payment,order and shipping screens

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+
+import { saveShippingAddress } from "../actions/cartActions";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+const ShippingScreen = ({ history }) => {
+    const cart = useSelector((state) => state.cart);
+    const { shippingAddress } = cart;
+
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [country, setCountry] = useState("");
+
+    useEffect(() => {
+        if (Object.keys(shippingAddress).lenght > 0) {
+            setAddress(shippingAddress.address);
+            setCity(shippingAddress.city);
+            setPostalCode(shippingAddress.postalCode);
+            setCountry(shippingAddress.country);
+        }
+    }, [shippingAddress]);
+
+    const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(saveShippingAddress({ address, city, postalCode, country }));
+        history.push("/payment");
+    };
+
+    return (
+        <FormContainer>
+            <CheckoutSteps step1 step2 />
+            <h1>Shipping</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId="address">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter address"
+                        value={address}
+                        required
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="city">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter city"
+                        value={city}
+                        required
+                        onChange={(e) => setCity(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="postalCode">
+                    <Form.Label>Postal Code</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter postal code"
+                        value={postalCode}
+                        required
+                        onChange={(e) => setPostalCode(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="country">
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter country"
+                        value={country}
+                        required
+                        onChange={(e) => setCountry(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Button
+                    type="submit"
+                    variant="outline-dark"
+                    className="rounded"
+                >
+                    Continue
+                </Button>
+            </Form>
+        </FormContainer>
+    );
+};
+
+export default ShippingScreen;
