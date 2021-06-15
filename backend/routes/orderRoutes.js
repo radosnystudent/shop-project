6992a7aaed52ceb834a90e898b26cb39314ac5e1
radosnystudent <jeremiasz.pauszek@gmail.com>
2021-06-15 13:00:02 +0200

    order route & add save items price in orderModel

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const Router = express.Router();
 
 Router.route("/").post(protect, addOrderItems);
+Router.route("/:id").get(protect, getOrderById);
 
 export default Router;
